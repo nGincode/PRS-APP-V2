f7fa18ebc519f0cd46aa9560d060f1c4758398d6
fembi19 <fembinurilham@gmail.com>
2022-07-19 13:09:50 +0700

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -51,18 +51,18 @@ class DatabaseSeeder extends Seeder
             'updated_at' => date('Y-m-d H:i:s')
 
         ]);
-        Store::create([
-            'nama' => 'CV Prima Rasa Selaras',
-            'active' => 1,
-            'tipe' => 'Outlet',
-            'alamat' => 'BENGKULU',
-            'img' => null,
-            'wa' => '085369957606',
-            'jam_kerja' => '[{"No":0,"Nama":"Shift 1","Masuk":"15:16","Pulang":"15:16"}]',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
+        // Store::create([
+        //     'nama' => 'CV Prima Rasa Selaras',
+        //     'active' => 1,
+        //     'tipe' => 'Outlet',
+        //     'alamat' => 'BENGKULU',
+        //     'img' => null,
+        //     'wa' => '085369957606',
+        //     'jam_kerja' => '[{"No":0,"Nama":"Shift 1","Masuk":"15:16","Pulang":"15:16"}]',
+        //     'created_at' => date('Y-m-d H:i:s'),
+        //     'updated_at' => date('Y-m-d H:i:s')
 
-        ]);
+        // ]);
 
 
         User::create([
@@ -85,25 +85,25 @@ class DatabaseSeeder extends Seeder
 
         ]);
 
-        User::create([
-            'store' => 'CV Prima Rasa Selaras',
-            'store_id' => 2,
-            'username' => 'prsprs',
-            'password' => bcrypt('prsprs'),
-            'email' => 'primarasaselaras@gmail.com',
-            'firstname' => 'PRS',
-            'lastname' => 'Prima Rasa Selaras',
-            'phone' => '085369957606',
-            'gender' => 1,
-            'izin' => 1,
-            'img' => null,
-            'last_login' => date('Y-m-d H:i:s'),
-            'email_verified_at' => null,
-            'remember_token' => null,
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s')
+        // User::create([
+        //     'store' => 'CV Prima Rasa Selaras',
+        //     'store_id' => 2,
+        //     'username' => 'prsprs',
+        //     'password' => bcrypt('prsprs'),
+        //     'email' => 'primarasaselaras@gmail.com',
+        //     'firstname' => 'PRS',
+        //     'lastname' => 'Prima Rasa Selaras',
+        //     'phone' => '085369957606',
+        //     'gender' => 1,
+        //     'izin' => 1,
+        //     'img' => null,
+        //     'last_login' => date('Y-m-d H:i:s'),
+        //     'email_verified_at' => null,
+        //     'remember_token' => null,
+        //     'created_at' => date('Y-m-d H:i:s'),
+        //     'updated_at' => date('Y-m-d H:i:s')
 
-        ]);
+        // ]);
 
         Groups::create([
             'nama' => 'SUPERADMIN',
