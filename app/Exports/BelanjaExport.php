60fc4533cde01e89d762053c91e3c43b8933a578
nGincode <fembinurilham@gmail.com>
2023-05-24 21:07:17 +0700

    fix

--- app/Exports/BelanjaExport.php
+++ app/Exports/BelanjaExport.php
@@ -81,7 +81,7 @@ class BelanjaExport implements
         $store = Store::where('id', $this->store)->first();
         return [
             [
-                strtoupper('LAPORAN Belanja ' . $store->nama . ' Tanggal ' . date('d/m/Y', strtotime("-1 day", strtotime($this->tgl_awal))) . ' - ' . date('d/m/Y', strtotime($this->tgl_akhir)))
+                strtoupper('LAPORAN Belanja ' . $store->nama . ' Tanggal ' . date('d/m/Y', strtotime($this->tgl_awal)) . ' - ' . date('d/m/Y', strtotime($this->tgl_akhir)))
             ],
             [],
             [
